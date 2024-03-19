5e6276a177aa2ce529db129092f5294679e91225
Matheus-Branco <112667064+Matheus-Branco@users.noreply.github.com>
2024-03-19 13:08:12 -0300

    Add files via upload

---
+++ Exercício07/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Exercíco07
+{
+    // Crie um programa para calcular o salário total de um vendedor. Deverá ser informado o salário base e o total de vendas.
+    // A comissão é calculada com um percentual (informado pelo usuário) sobre o total de vendas.
+    // salário total = salário base + (total de vendas * comissão/100)
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para calcular salário total de um vendedor");
+
+            Console.WriteLine("Qual o nome do vendedor?");
+            String nome = Console.ReadLine();
+
+            Console.WriteLine("Para isso preciso que nos informe o salário base do vendedor");
+            string sal_baseS = Console.ReadLine();
+            int sal_base = Convert.ToInt32(sal_baseS);
+
+            Console.WriteLine("Para isso preciso que nos informe o total de vendas do vendedor");
+            string tot_venS = Console.ReadLine();
+            int tot_ven = Convert.ToInt32(tot_venS);
+
+            Console.WriteLine("Para isso preciso que nos informe a porcentual da comissão do vendedor");
+            string comS = Console.ReadLine();
+            int com = Convert.ToInt32(comS);
+
+            int sal_tot = sal_base + (tot_ven * com / 100);
+
+            Console.WriteLine("O salário total do vendedor " + nome + " é de " + sal_tot);
+        }
+    }
+}
