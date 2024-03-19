9f6dd9d209c827cb5aa88f2d5298b7e8751e3c11
Matheus-Branco <112667064+Matheus-Branco@users.noreply.github.com>
2024-03-19 13:09:44 -0300

    Add files via upload

---
+++ Exercício09/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml;
+
+namespace Exercício09
+{
+    //Crie um programa para calcular a média harmônica das notas de um Aluno
+    //n / nota01 + nota02 + notaN 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite quatro notas dos alunos:");
+            decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+            decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+            decimal nota3 = Convert.ToDecimal(Console.ReadLine());
+            decimal nota4 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal mediaHarmonica = 4 / ((1 /nota1) + (1/nota2) + (1/nota3) + (1/nota4));
+
+            Console.WriteLine("A média harmônica das notas é: " + mediaHarmonica);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercício10/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercíco10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a nota da primeira prova: ");
+            decimal nota1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe o peso da primeira prova: ");
+            decimal peso1 = Convert.ToDecimal(Console.ReadLine());
+            
+            Console.WriteLine("Informe a nota da segunda prova: ");
+            decimal nota2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Informe o peso da segunda prova: ");
+            decimal peso2 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal mediaPonderada = (nota1 * peso1 + nota2 * peso2) / (peso1 + peso2);
+
+            Console.ReadLine();
+        }
+    }
+}
